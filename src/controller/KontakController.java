1fd6f5fe67aebced740d08fa3eb0fd61d3691649
syarif <syarifridwan433@gmail.com>
2025-11-02 14:31:25 +0700

    membuat class kontak

--- src/controller/KontakController.java
+++ src/controller/KontakController.java
@@ -2,12 +2,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package controller;
 
+package controller;
+import model.Kontak;
+import model.KontakDAO;
+import java.sql.SQLException;
+import java.util.List;
 /**
  *
  * @author SYARIF
  */
 public class KontakController {
-    
+
+    private final KontakDAO contactDAO;
+
+    public KontakController() {
+        contactDAO = new KontakDAO();
+    }
+
+    // Mengambil semua data kontak
+    public List<Kontak> getAllContacts() throws SQLException {
+        return contactDAO.getAllContacts();
+    }
+
+    // Menambah kontak
+    public void addContact(String nama, String nomorTelepon, String kategori) throws SQLException {
+        Kontak contact = new Kontak(0, nama, nomorTelepon, kategori);
+        contactDAO.addContact(contact);
+    }
+
+    // Mengupdate kontak
+    public void updateContact(int id, String nama, String nomorTelepon, String kategori) throws SQLException {
+        Kontak contact = new Kontak(id, nama, nomorTelepon, kategori);
+        contactDAO.updateContact(contact);
+    }
+
+    // Menghapus kontak
+    public void deleteContact(int id) throws SQLException {
+        contactDAO.deleteContact(id);
+    }
+
+    // Pencarian kontak
+    public List<Kontak> searchContacts(String keyword) throws SQLException {
+        return contactDAO.searchContacts(keyword);
+    }
+
+    // Cek duplikasi nomor telepon (excludeId opsional saat update)
+    public boolean isDuplicatePhoneNumber(String nomorTelepon, Integer excludeId) throws SQLException {
+        return contactDAO.isDuplicatePhoneNumber(nomorTelepon, excludeId);
+    }
 }
